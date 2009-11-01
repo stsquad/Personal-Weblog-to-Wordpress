d80b99f77f42065bfef817025eb079c0f31c9e9d
Alex Bennee <alex@bennee.com>
2009-11-01 20:21:03 +0100

    Fixed inserting of meta tags

--- convert_to_wp.py
+++ convert_to_wp.py
@@ -8,14 +8,18 @@
 # License: GPLv3 (or later)
 
 import getopt
+import sys
+import MySQLdb
 
-
+# Params
 wp_prefix="wp_"
 database=""
 username=""
 password=""
 
+# Mappings
 id_to_tag={}
+pwid_to_wpid={}
 articles=[]
 
 def do_conversion():
@@ -28,12 +32,17 @@ def do_conversion():
         if r == None:
             break
         tid = r[0]
-        name = lower(r[1])
+        name = r[1].lower()
         id_to_tag[tid] = name
 
     # Create the meta tags in wordpress
-    for k, v in id_to_tag:
-        c.execute("INSERT into "+wp_prefix+"terms (name, slug) VALUES ('"+v+", "+v+")")
+    for k, v in id_to_tag.iteritems():
+        ins_sql="INSERT into "+wp_prefix+"terms (name, slug) VALUES ('"+v+"', '"+v+"')"
+        print "SQL: %s" % (ins_sql)
+        c.execute(ins_sql)
+        c.execute("SELECT term_id FROM "+wp_prefix+"terms WHERE name='"+v+"'")
+        wp_key = c.fetchone()
+        print "added meta key: %s as WP term %d", v, wp_key[0]
         
 
         
@@ -41,18 +50,16 @@ def do_conversion():
     c.execute("SELECT title, tid, updated, body, more FROM entries")
     while (1):
         r = c.fetchone()
-        if r = None:
+        if r == None:
             break
         title=r[0]
         post=r[3]+r[4]
-        tag=id_to_tag[r[1])
+        tag=id_to_tag(r[1])
         
         
 
 # Start of code
-if __name__ == ~.~.
-
-"__main__":
+if __name__ == "__main__":
     try:
         opts, args = getopt.getopt(sys.argv[1:], "u:p:d:", ["user=", "db=", "pw="])
     except getopt.GetoptError, err:
